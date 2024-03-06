02da8d774018c12352b9edf963a8630d542d638c
esraelif <elifesratunca@gmail.com>
2024-03-06 12:24:46 +0100

    GorevEkleAdded

--- src/components/GorevEkle.jsx
+++ src/components/GorevEkle.jsx
@@ -1,35 +1,76 @@
-import React, { useState } from 'react';
-import { Button, Form, InputGroup } from 'react-bootstrap';
-import { uid } from 'uid';
+import { useState } from "react";
 
-const Header = ({ todos, setTodos }) => {
-    const [task, setTask] = useState("")
-    const addTodo = () => {
-        console.log({ task })
-        const newTodo = {
-            id: uid(),
-            text: task,
-            completed: false,
-        }
-        console.log(newTodo)
-        setTodos([...todos, newTodo])
-        setTask("")
+const GorevEkle = ({ todos, setTodos }) => {
+    const [texT, setText] = useState("")
+    const [day, setDay] = useState("")
+    const [display, setDisplay] = useState(true)
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const id = Math.ceil(Math.random() * 100) + 6;
+
+        //1.yol kalıcı kaydedilmeyen
+        // setTodos([{id:id,text:texT,day:day,isDone:false},...todos])
+        //2.yol localstoroge nin ilk yolu
+        const newTodos = { id: id, text: texT, day: day, isDone: false };
+        //*     todos = [newTodos, ...todos];
+        //  setTodos(todos);
+        //  localStorage.setItem("gorevler",JSON.stringify(todos))
+
+        // 3.yol (localsoroge a yollamanın 2.yolu)
+
+        localStorage.setItem("gorevler", JSON.stringify([...todos, newTodos]))
+        setTodos(JSON.parse(localStorage.getItem("gorevler")))
+
+        // input alanlarını boşaltmak için, altta inputlara value veriyoruz, burada da temizliyoruz
+        setText("")
+        setDay("")
     }
     return (
         <div>
-            <h1 className='text-center text-danger m-5'>Todo App</h1>
-            <InputGroup className="mb-3 w-50 d-flex mx-auto">
-                <Form.Control
-                    placeholder="Enter new todo..."
-                    aria-label="Recipient's username"
-                    aria-describedby="basic-addon2"
-                    value={task}
-                    onChange={(e) => setTask(e.target.value)}
-                />
-                <Button className='input-group-text bg-success' disabled={!task.trim()} id="basic-addon2" onClick={addTodo}>Add Todo</Button>
-            </InputGroup>
+            <header className="header">
+                <h1>TO DO APP</h1>
+                <button
+                    className="btn"
+                    style={{ background: display ? "red" : "purple" }}
+                    onClick={() => setDisplay(!display)}
+                >
+                    {display ? "CLOSE" : "SHOW"} ADD TASK BAR
+                </button>
+            </header>
+
+            {display && <form onSubmit={handleSubmit}>
+                <div className="form-control">
+                    <label htmlFor="text">Task</label>
+                    <input
+                        id="text"
+                        type="text"
+                        name="text"
+                        onChange={(e) => setText(e.target.value)}
+                        value={texT}
+                    />
+
+                </div>
+                <div className="form-control">
+                    <label htmlFor="day">Day & Time</label>
+                    <input
+                        id="day"
+                        type="datetime-local"
+                        onChange={(e) => setDay(e.target.value)}
+                        name="day"
+                        value={day}
+                    />
+                </div>
+                <div>
+                    <button className="btn btn-submit" type="submit">
+                        SUBMİT
+                    </button>
+                </div>
+            </form>}
+
+
         </div>
     );
-}
+};
 
-export default Header;
+export default GorevEkle;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,76 +1,19 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import GorevEkle from "../components/GorevEkle";
+import GorevleriGoster from "../components/GorevleriGoster";
 
-const GorevEkle = ({ todos, setTodos }) => {
-    const [texT, setText] = useState("")
-    const [day, setDay] = useState("")
-    const [display, setDisplay] = useState(true)
+import Data from "../helper/Data"
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const id = Math.ceil(Math.random() * 100) + 6;
+const Home = () => {
 
-        //1.yol kalıcı kaydedilmeyen
-        // setTodos([{id:id,text:texT,day:day,isDone:false},...todos])
-        //2.yol localstoroge nin ilk yolu
-        const newTodos = { id: id, text: texT, day: day, isDone: false };
-        //*     todos = [newTodos, ...todos];
-        //  setTodos(todos);
-        //  localStorage.setItem("gorevler",JSON.stringify(todos))
+    const [todos, setTodos] = useState(JSON.parse(localStorage.getItem("gorevler")) || Data)
 
-        // 3.yol (localsoroge a yollamanın 2.yolu)
-
-        localStorage.setItem("gorevler", JSON.stringify([...todos, newTodos]))
-        setTodos(JSON.parse(localStorage.getItem("gorevler")))
-
-        // input alanlarını boşaltmak için, altta inputlara value veriyoruz, burada da temizliyoruz
-        setText("")
-        setDay("")
-    }
     return (
         <div>
-            <header className="header">
-                <h1>TO DO APP</h1>
-                <button
-                    className="btn"
-                    style={{ background: display ? "red" : "purple" }}
-                    onClick={() => setDisplay(!display)}
-                >
-                    {display ? "CLOSE" : "SHOW"} ADD TASK BAR
-                </button>
-            </header>
-
-            {display && <form onSubmit={handleSubmit}>
-                <div className="form-control">
-                    <label htmlFor="text">Task</label>
-                    <input
-                        id="text"
-                        type="text"
-                        name="text"
-                        onChange={(e) => setText(e.target.value)}
-                        value={texT}
-                    />
-
-                </div>
-                <div className="form-control">
-                    <label htmlFor="day">Day & Time</label>
-                    <input
-                        id="day"
-                        type="datetime-local"
-                        onChange={(e) => setDay(e.target.value)}
-                        name="day"
-                        value={day}
-                    />
-                </div>
-                <div>
-                    <button className="btn btn-submit" type="submit">
-                        SUBMİT
-                    </button>
-                </div>
-            </form>}
-
-
+            <GorevEkle todos={todos} setTodos={setTodos} />
+            <GorevleriGoster todos={todos} setTodos={setTodos} />
         </div>
     );
 };
 
-export default GorevEkle;
+export default Home;
