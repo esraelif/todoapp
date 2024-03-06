64f649f6f1348a6af0c06a96fdf97dee49ae08cb
esraelif <elifesratunca@gmail.com>
2024-03-06 09:23:24 +0100

    pagesAdded

---
+++ src/components/GorevEkle.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { Button, Form, InputGroup } from 'react-bootstrap';
+import { uid } from 'uid';
+
+const Header = ({ todos, setTodos }) => {
+    const [task, setTask] = useState("")
+    const addTodo = () => {
+        console.log({ task })
+        const newTodo = {
+            id: uid(),
+            text: task,
+            completed: false,
+        }
+        console.log(newTodo)
+        setTodos([...todos, newTodo])
+        setTask("")
+    }
+    return (
+        <div>
+            <h1 className='text-center text-danger m-5'>Todo App</h1>
+            <InputGroup className="mb-3 w-50 d-flex mx-auto">
+                <Form.Control
+                    placeholder="Enter new todo..."
+                    aria-label="Recipient's username"
+                    aria-describedby="basic-addon2"
+                    value={task}
+                    onChange={(e) => setTask(e.target.value)}
+                />
+                <Button className='input-group-text bg-success' disabled={!task.trim()} id="basic-addon2" onClick={addTodo}>Add Todo</Button>
+            </InputGroup>
+        </div>
+    );
+}
+
+export default Header;
